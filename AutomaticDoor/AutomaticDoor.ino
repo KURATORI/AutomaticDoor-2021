7e3d6ed9c3b3c807237318fbfb03755e131098f2
くら鳥_OPUCT <57042736+KURATORI@users.noreply.github.com>
2021-10-20 15:19:47 +0900

    誤字修正

--- AutomaticDoor/AutomaticDoor.ino
+++ AutomaticDoor/AutomaticDoor.ino
@@ -2,12 +2,12 @@
 #include"Temperature.h"
 #include"Motor.h"
 #include"Usound.h"
-#define PIR_PIN 0
-#define MOT_INPULSE_PIN 1
-#define MOT_ROTDIRECTION_PIN 2
-#define MOT_OUTPUlSE_PIN 3
-#define ECHO_PIN 4
-#define TRIG_PIN 5
+#define PIR_PIN 2
+#define MOT_INPULSE_PIN 3
+#define MOT_ROTDIRECTION_PIN 4
+#define MOT_OUTPULSE_PIN 5
+#define ECHO_PIN 6
+#define TRIG_PIN 7
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
@@ -15,7 +15,7 @@ void setup() {
   pinMode(PIR_PIN, INPUT);
   pinMode(MOT_INPULSE_PIN, INPUT);
   pinMode(MOT_ROTDIRECTION_PIN, OUTPUT);
-  pinMode(MOT_OUTPUlSE_PIN, OUTPUT);
+  pinMode(MOT_OUTPULSE_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
   
@@ -24,40 +24,59 @@ void setup() {
   Serial.println("test");  
   mlx.begin();  //実行しないとデフォルト値が出続ける
 }
+Usound US(ECHO_PIN, TRIG_PIN);
 
 void loop() {
 
   //超音波センサ実行プログラム
-  /*
-  Usound US(ECHO_PIN, TRIG_PIN);
+  
+  
   Serial.print("distance: ");
   Serial.print(US.echoCatch());
-  Serial.println("mm");
-  *
+  Serial.println("cm");
+  
   
   //モータの実行プログラム
   /*
   Motor M(MOT_INPULSE_PIN, MOT_ROTDIRECTION_PIN, MOT_OUTPUlSE_PIN);
-  bool rotdir = 1;
-  float rps = 1000;
+  bool rotdir = 0;
+  float rps = 100;
   float meter = 100;
   M.rotateMotor(rotdir, rps, meter);
   */
+  
+    Serial.println(10);
+    digitalWrite(MOT_ROTDIRECTION_PIN,1);
+    for(int i=0;i<1000;i++){
+      digitalWrite(MOT_OUTPULSE_PIN,1);
+      delay(1);
+      digitalWrite(MOT_OUTPULSE_PIN,0);
+      delay(1);
+    }
+    digitalWrite(MOT_ROTDIRECTION_PIN,0);
+    for(int i=0;i<1000;i++){
+      digitalWrite(MOT_OUTPULSE_PIN,1);
+      delay(1);
+      digitalWrite(MOT_OUTPULSE_PIN,0);
+      delay(1);
+    }
+  
+  
  
   //PIRセンサの実行プログラム
-  /*
+  
   if(digitalRead(PIR_PIN) == HIGH)Serial.println("HIGH");
   else Serial.println("Low");
-  */
+  
 
   //温度センサの実行プログラム
   /*
   Temperature T;
   Serial.print("Ambient = ");
-  Serial.print(T.getObjectTemperature()); 
+  Serial.print(T.getAmbientTemperature()); 
   Serial.println("*C");
   Serial.print("Object = ");
-  Serial.print(T.getAmbientTemperature());
+  Serial.print(T.getObjectTemperature());
   Serial.println("*C");
   Serial.println();
   delay(1000);
--- AutomaticDoor/Usound.cpp
+++ AutomaticDoor/Usound.cpp
@@ -5,16 +5,16 @@ Usound::Usound(int pin1, int pin2){
   trig_pin = pin2;
 }
 
-float Usound::echoCatch(){
+double Usound::echoCatch(){
   digitalWrite(trig_pin, LOW);
   delayMicroseconds(2);
   digitalWrite(trig_pin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig_pin, LOW);
-  duration = pulseIn(trig_pin, LOW); //パルスが入力された際にその時間をμs単位で計測
+  duration = pulseIn(echo_pin, HIGH); //パルスが入力された際にその時間をμs単位で計測
   if(duration > 0){
     duration = duration / 2;
-    distance = duration * 340 /1000000; //センサからの距離(mm)を代入
+    distance = duration * 340 * 100 /1000000 ; //センサからの距離(cm)を代入
     return  distance;
   }
 }
